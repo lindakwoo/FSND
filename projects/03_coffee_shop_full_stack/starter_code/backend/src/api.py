6bf3cc07bc7085a711800285f6ad861fc97c69f7
Linda Woo <lindakwoo@gmail.com>
2020-05-28 13:52:27 -0700

    fixed api.py to adhere to PEP 8 style guide

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -12,51 +12,59 @@ setup_db(app)
 CORS(app)
 
 
-# undo comments below to clear all data in database and then seed it with two drinks:
+# undo comments below to clear all data in database 
+# and then seed it with two drinks:
 
 # db_drop_and_create_all()
 # recipe = [{"color": "orange", "name":"orange juice", "parts":1}]
 # recipe_str = json.dumps(recipe)
 # drink = Drink(title = "Wooster", recipe = recipe_str)
 # drink.insert()
-# recipe = [{"color": "blue", "name":"blueberries", "parts":2},{"color": "yellow", "name":"lemons", "parts":1}]
+# recipe = [{"color": "blue", "name":"blueberries", "parts":2},
+# {"color": "yellow", "name":"lemons", "parts":1}]
 # recipe_str = json.dumps(recipe)
 # drink = Drink(title = "drinkipoo", recipe = recipe_str)
 # drink.insert()
 
-## ROUTES
+# ROUTES
 
-# get '/drinks' requires no authorization and displays drinks upon opening the website.
+# get '/drinks' requires no authorization and displays 
+# drinks upon opening the website.
+
 
 @app.route('/drinks', methods=['GET'])
 def get_drinks():
     try:
-        drinks = [drink.short() for drink in Drink.query.order_by(Drink.id).all()]
+        drinks = [drink.short() for drink in 
+        Drink.query.order_by(Drink.id).all()]
         return jsonify({
             "success": True,
             "drinks": drinks
         })     
-  
     except Exception as e:
         print(e)
         abort(422)     
 
 
-#get '/drinks-detail' needs authorization and allows user to click on drinks to see details
-@app.route('/drinks-detail', methods = ["GET"])
+# get '/drinks-detail' needs authorization and allows 
+# user to click on drinks to see details
+@app.route('/drinks-detail', methods=["GET"])
 @requires_auth('get:drinks-detail')
 def get_long_drinks(jwt):
     try:
-        drinks = [drink.long() for drink in Drink.query.order_by(Drink.id).all()]
+        drinks = [drink.long() for drink in 
+        Drink.query.order_by(Drink.id).all()]
         return jsonify({
             "success": True,
             "drinks": drinks
         })
-    except:
+    except Exception:
         abort(422)      
 
-#post '/drinks' needs authorization and allows user to create new drink and add to database
-@app.route('/drinks', methods = ["POST"])
+
+# post '/drinks' needs authorization and allows user to create 
+# new drink and add to database
+@app.route('/drinks', methods=["POST"])
 @requires_auth('post:drinks')
 def create_drink(jwt):
     if request.data:
@@ -73,8 +81,9 @@ def create_drink(jwt):
     else:
         abort(422)
 
-#patch '/drinks/<id>' needs authorization and allows user to alter drinks details and save changes to database
-@app.route('/drinks/<id>', methods = ["PATCH"])
+# patch '/drinks/<id>' needs authorization and allows user to alter 
+# drinks details and save changes to database
+@app.route('/drinks/<id>', methods=["PATCH"])
 @requires_auth('patch:drinks')
 def change_drink(jwt, id):
     if request.data:
@@ -84,7 +93,7 @@ def change_drink(jwt, id):
         drink = Drink.query.filter(Drink.id == id).one_or_none()
         if drink is None:
             abort(404)
-        else:    
+        else:
             drink.title = title
             drink.recipe = json.dumps(recipe)
             drink.update()
@@ -95,42 +104,43 @@ def change_drink(jwt, id):
     else:
         abort(422)
 
-#delete '/drinks/<id>' needs authorization and allows user to delete a drink from the database
-@app.route('/drinks/<id>', methods = ["DELETE"])
+# delete '/drinks/<id>' needs authorization and allows user to delete 
+# a drink from the database
+@app.route('/drinks/<id>', methods=["DELETE"])
 @requires_auth('delete:drinks')
 def delete_drink(jwt, id):
     try:
         drink = Drink.query.filter(Drink.id == id).one_or_none()
         if drink is None:
             abort(404)
-        else:    
+        else:
             drink.delete()
             return jsonify({
                 'success': True,
                 'delete': id
             })
-    except:
-      abort(422)         
-   
+    except Exception:
+        abort(422)         
 
-## Error Handling
+# Error Handling
 
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
-                    "error": 422,
-                    "message": "unprocessable"
-                    }), 422
-               
+        "success": False, 
+        "error": 422,
+        "message": "unprocessable"
+        }), 422
+    
 
 @app.errorhandler(404)
 def resource_not_found(error):
     return jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
+        "success": False, 
+        "error": 404,
+        "message": "resource not found"
+        }), 404
+
 
 @app.errorhandler(400)
 def bad_request(error):
@@ -140,6 +150,7 @@ def bad_request(error):
         "message": "bad request"
         }), 400
 
+
 @app.errorhandler(405)
 def bad_method(error):
     return jsonify({
@@ -147,6 +158,7 @@ def bad_method(error):
         "error": 405,
         "message": "wrong method"
         }), 405
+
 
 @app.errorhandler(500)
 def server_error(error):
@@ -156,6 +168,7 @@ def server_error(error):
         "message": "internal server error"
         }), 500
 
+
 @app.errorhandler(401)
 def server_error(error):
     return jsonify({
@@ -163,6 +176,7 @@ def server_error(error):
         "error": 401,
         "message": "invalid authorization"
         }), 401
+
 
 @app.errorhandler(AuthError)
 def auth_error(e):
